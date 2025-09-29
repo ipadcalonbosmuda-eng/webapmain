afc95dd17a4e2e9e3a98739ffc5a3e80470abf2e
ipadcalonbosmuda-eng <ipadcalonbosmuda@gmail.com>
2025-09-29 14:36:43 +0700

    ui(home): change 'View Explorer' CTA to 'View Docs' and link to docs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,12 +61,12 @@ export default function HomePage() {
               <ArrowRight className="ml-2 h-5 w-5" />
             </Link>
             <a
-              href="https://plasmascan.to"
+              href="https://docs.plasma.to"
               target="_blank"
               rel="noopener noreferrer"
               className="btn-secondary text-lg px-8 py-3 inline-flex items-center justify-center"
             >
-              View Explorer
+              View Docs
             </a>
           </div>
         </div>
